b0483f6611e7a296b1b2a45c8ac9d9cca2655a22
cybedra <147957633+cybedra@users.noreply.github.com>
2026-02-26 16:33:16 -0800

    TODO: test in lab, works with fake board

--- collect_data.py
+++ collect_data.py
@@ -7,64 +7,75 @@ from threading import Thread, Event
 from queue import Queue
 
 import serial
+from serial import Serial
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
-from psychopy import core
 import psychopy.visual
-import psychopy.event
 
 import numpy as np
-from scipy import signal
-import pickle
 import mne
 
-width = 1536
-height = 864
-aspect_ratio = width/height
-
-CYTON_SAMPLING_RATE = 250  # Hz
+# things to change
+WIDTH = 1536               # px
+HEIGHT = 864               # px
 RECORDING_DURATION = 4.5   # seconds
-BASELINE_DURATION = 1
-PREP_DURATION = 5
-NUM_CHANNELS = 8
+BASELINE_DURATION = 5      # seconds
+PREP_DURATION = 5          # seconds
+BREAK_DURATION = 0.75      # seconds
+SES_NUMBER = 2
+NUM_TRIALS = 5
+DATA_DIR = "data/2-26"
+FAKE_BOARD = True
 
+# things not NOT change
+CYTON_SAMPLING_RATE = 250  # Hz
+NUM_CHANNELS = 8
 FILTER_LOW_FREQ = 8        # Hz
 FILTER_HIGH_FREQ = 30      # Hz
-
 CYTON_BOARD_ID = 0
 BAUD_RATE = 115200
 ANALOGUE_MODE = '/2'
 
-SES_NUMBER = 1
-NUM_TRIALS = 64
-DATA_DIR = "data/2-25"
-
-SYNTHETIC_BOARD = True
-
 win = psychopy.visual.Window(
-    size=(width, height),
-    units="norm",
-    checkTiming = True,
-    allowGUI = False,
-    fullscr=False)
+        size=(WIDTH, HEIGHT),
+        units="norm",
+        checkTiming = True,
+        allowGUI = False,
+        fullscr=False
+    )
 
 text = psychopy.visual.TextStim(win=win, height=0.145, color='white', units='norm', font="Helvetica")
 
-def create_photosensor_dot(window, size=2/8*0.7):
-    width, height = window.size
-    ratio = width/height
+# psychopy helpers (i hate psychopy bruh)
+def init_photosensor_dot(window, size=2/8*0.7):
+    w, h = window.size
+    ratio = w/h
     return psychopy.visual.Rect(win=window, units="norm", width=size, height=size * ratio, 
                         fillColor='white', lineWidth = 0, pos = [1 - size/2, -1 - size/8]
     )
+
+photosensor_dot = init_photosensor_dot(win)
     
-def init_window(win, dot):
-    dot.color = np.array([-1, -1, -1])
-    dot.draw()
-    
+def init_window():
+    # draw dot
+    photosensor_dot.color = np.array([-1, -1, -1])
+    photosensor_dot.draw()
+    # draw some text
     text.text = "init"
     text.draw()
+    # ~~draw~~ flip window
+    win.flip()
+
+def change_window(color_arr, s=None):
+    photosensor_dot.color = np.array(color_arr)
+    photosensor_dot.draw()
+    
+    if s is not None:
+        text.text = s
+        text.draw()
     
     win.flip()
 
+# openbci/Cyton interfacing helpers
 def find_openbci_port():
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
@@ -98,18 +109,21 @@ def find_openbci_port():
     return openbci_port
 
 def init_cyton(serial_port=None):
-    print("Initializing Cyton")
+    print("Initializing Cyton...")
     params = BrainFlowInputParams()
-    if not SYNTHETIC_BOARD and CYTON_BOARD_ID != 6:
-        params.serial_port = serial_port
-    elif CYTON_BOARD_ID == 6:
-        params.ip_port = 9000
     
-    # board = BoardShim(CYTON_BOARD_ID, params)
-    board_id = BoardIds.SYNTHETIC_BOARD.value
-    board = BoardShim(board_id, params)
+    if FAKE_BOARD or CYTON_BOARD_ID == 6:
+        print("Board is synthetic.")
+        params.ip_port = 9000
+        board_id = BoardIds.SYNTHETIC_BOARD.value
+        board = BoardShim(board_id, params)
+    else:
+        print("Board is real.")
+        params.serial_port = serial_port
+        board = BoardShim(CYTON_BOARD_ID, params)
 
     board.prepare_session()
+    print("Configuring Cyton...")
     
     res = board.config_board('/0')
     print(res)
@@ -118,7 +132,7 @@ def init_cyton(serial_port=None):
     res = board.config_board(ANALOGUE_MODE)
     print(res)
 
-    print("Init done.")
+    print("Cyton setup done.")
     return board
 
 def record_bl(board):
@@ -131,7 +145,6 @@ def record_bl(board):
     eeg = data[eeg_channels]
     
     board.stop_stream()
-    
     print("Recording done.")
     return eeg
 
@@ -145,10 +158,20 @@ def record_eeg(board):
     eeg = data[eeg_channels]
     
     board.stop_stream()
-    
     print("Recording done.")
     return eeg
 
+def get_data(board, queue_in, stop_event):
+    while not stop_event.is_set():
+        data_in = board.get_board_data()
+        eeg_in = data_in[board.get_eeg_channels(CYTON_BOARD_ID)]
+        aux_in = data_in[board.get_analog_channels(CYTON_BOARD_ID)]
+        timestamp_in = data_in[board.get_timestamp_channel(CYTON_BOARD_ID)]
+        if len(timestamp_in) > 0:
+            queue_in.put((eeg_in, aux_in, timestamp_in))
+        time.sleep(0.1)
+
+# data processing helpers
 def filter_eeg(raw_eeg):
     # Butterworth band pass filter
     filtered_eeg = mne.filter.filter_data(
@@ -163,21 +186,11 @@ def filter_eeg(raw_eeg):
 
     return filtered_eeg
 
-def get_data(board, queue_in, stop_event):
-    while not stop_event.is_set():
-        data_in = board.get_board_data()
-        eeg_in = data_in[board.get_eeg_channels(CYTON_BOARD_ID)]
-        aux_in = data_in[board.get_analog_channels(CYTON_BOARD_ID)]
-        timestamp_in = data_in[board.get_timestamp_channel(CYTON_BOARD_ID)]
-        if len(timestamp_in) > 0:
-            queue_in.put((eeg_in, aux_in, timestamp_in))
-        time.sleep(0.1)
-
+# main script
 if __name__ == "__main__":
-    photosensor_dot = create_photosensor_dot(win)
-    init_window(win, photosensor_dot)
+    init_window()
     
-    if SYNTHETIC_BOARD:
+    if FAKE_BOARD:
         board = init_cyton()
     else:
         port = find_openbci_port()
@@ -196,77 +209,78 @@ if __name__ == "__main__":
     recordings = []
     recordings_filtered = []
     
+    black = [-1, -1, -1]
+    white = [1, 1, 1]
+    
     os.makedirs(DATA_DIR, exist_ok=True)
     
     # movements = ["stomp left", "stomp right", "flick tongue", "no action"]
     movements = ["stomp right", "no action"]
     try:
         # record a baseline
-        print("preparing to record baseline...")
+        change_window(black, f"collecting baseline in {str(PREP_DURATION)}")
+        print("Preparing to record baseline...")
         time.sleep(PREP_DURATION)
+        
+        change_window(white, "baseline")
+        
         raw_data = record_bl(board)
         baseline.append(("no action", raw_data))
+        
+        change_window(black)
         
         board.start_stream(45000)
         cyton_thread = Thread(target=get_data, args=(board, queue_in, stop_event), daemon=True)
         cyton_thread.start()
         
         # run trials
-        print("preparing to run trials...")
+        print("Preparing to run trials...")
+        change_window(black, f"running trials in {str(PREP_DURATION)}")
         time.sleep(PREP_DURATION)
         
-        print("running trials")
         for i in range(NUM_TRIALS):
             trial_movement = movements[random.randint(0, len(movements) - 1)]
-
-            # intertrial, draw label, black dot
-            text.text = trial_movement
-            text.draw()
-            photosensor_dot.color = np.array([-1, -1, -1])
-            photosensor_dot.draw()
-            win.flip()
-            time.sleep(0.75)
-
-            print(f"trial {i}: {trial_movement}")
-            # white dot
-            text.text = trial_movement
-            text.draw()
-            photosensor_dot.color = np.array([1, 1, 1])
-            photosensor_dot.draw()
-            win.flip()
+            
+            # intertrial: black dot, draw movement to screen
+            change_window(black, trial_movement)
+            time.sleep(BREAK_DURATION)
+            # trial: white dot
+            print(f"Trial {i+1}/{NUM_TRIALS}: {trial_movement}")
+            change_window(white, trial_movement)
             time.sleep(RECORDING_DURATION)
-
-            # black dot again
-            photosensor_dot.color = np.array([-1, -1, -1])
-            photosensor_dot.draw()
-            win.flip()
-
-            # process data
-            if SYNTHETIC_BOARD:
+            # posttrial: black dot, remove movement to prep for next one
+            change_window(black)
+            
+            # data collection
+            if FAKE_BOARD:
                 while not queue_in.empty():
-                    print("reading fake data stream")
                     eeg_in, aux_in, timestamp_in = queue_in.get()
                     eeg_continuous = np.concatenate((eeg_continuous, eeg_in), axis=1)
                     aux_continuous = np.concatenate((aux_continuous, aux_in), axis=1)
+                
+                print("Adding trial to recordings...")
                 trial_samples = int(RECORDING_DURATION * CYTON_SAMPLING_RATE)
                 raw_data = np.zeros((NUM_CHANNELS, trial_samples)) 
+                recordings.append((trial_movement, raw_data))
+                print(f"Trial {i+1}/{NUM_TRIALS} ({trial_movement}): eeg shape {raw_data.shape} saved.")
             else:
                 while len(trial_ends) <= i:
-                    print("reading data stream")
                     while not queue_in.empty():
                         eeg_in, aux_in, timestamp_in = queue_in.get()
                         eeg_continuous = np.concatenate((eeg_continuous, eeg_in), axis=1)
                         aux_continuous = np.concatenate((aux_continuous, aux_in), axis=1)
+                    # this print for debugging
+                    print(f"aux ch1 range: min={aux_continuous[1].min():.2f} max={aux_continuous[1].max():.2f}")
                     photo_trigger = (aux_continuous[1] > 20).astype(int)
-                    trial_starts = np.where(np.diff(photo_trigger) == 1)[0]
-                    trial_ends = np.where(np.diff(photo_trigger) == -1)[0]
+                    trial_starts = np.where(np.diff(photo_trigger) >= 0.9)[0]
+                    trial_ends = np.where(np.diff(photo_trigger) <= -0.9)[0]
 
-                print("adding trial to recordings")
+                print("Adding trial to recordings...")
                 trial_samples = int(RECORDING_DURATION * CYTON_SAMPLING_RATE)
                 trial_start_sample = trial_starts[i]
                 raw_data = np.copy(eeg_continuous[:, trial_start_sample:trial_start_sample + trial_samples])
                 recordings.append((trial_movement, raw_data))
-                print(f"Trial {i+1}/{NUM_TRIALS} ({trial_movement}): eeg shape {raw_data.shape}")
+                print(f"Trial {i+1}/{NUM_TRIALS} ({trial_movement}): eeg shape {raw_data.shape} saved.")
     finally:
         stop_event.set()
         board.stop_stream()
@@ -280,16 +294,23 @@ if __name__ == "__main__":
     filtered_data = filter_eeg(baseline[0][1])
     baseline_filtered.append((baseline[0][0], filtered_data))
 
-    
     # save to files
     base_output = np.array(baseline, dtype=object)
     np.save(f"{DATA_DIR}/baseline-session-{SES_NUMBER}.npy", base_output)
+    print(f"{DATA_DIR}/baseline-session-{SES_NUMBER} saved.")
+    
     filtered_base_output = np.array(baseline_filtered, dtype=object)
     np.save(f"{DATA_DIR}/filtered-baseline-session-{SES_NUMBER}.npy", filtered_base_output)
+    print(f"{DATA_DIR}/filtered-baseline-session-{SES_NUMBER} saved.")
+    
     raw_output = np.array(recordings, dtype=object)
     np.save(f"{DATA_DIR}/raw-session-{SES_NUMBER}.npy", raw_output)
+    print(f"{DATA_DIR}/raw-session-{SES_NUMBER} saved.")
+    
     filtered_output = np.array(recordings_filtered, dtype=object)
     np.save(f"{DATA_DIR}/filtered-session-{SES_NUMBER}.npy", filtered_output)
+    print(f"{DATA_DIR}/filtered-session-{SES_NUMBER} saved.")
 
     np.save(f"{DATA_DIR}/eeg-continuous-session-{SES_NUMBER}.npy", eeg_continuous)
     np.save(f"{DATA_DIR}/aux-continuous-session-{SES_NUMBER}.npy", aux_continuous)
+    print("All done!")
